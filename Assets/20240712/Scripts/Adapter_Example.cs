cceddaa7e1b56064e3db2fbf3139cfc34c2be178
Jiwon Park <64686238+tenvi34@users.noreply.github.com>
2024-07-12 15:51:42 +0900

    Adapter 패턴 내용 학습

--- Assets/20240712/Scripts/Adapter_Example.cs
+++ Assets/20240712/Scripts/Adapter_Example.cs
@@ -1,9 +1,34 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// sealed -> 더이상 상속받지 못하게 할 때
+public sealed class Tway
+{
+    public void Fly()
+    {
+        
+    }
+}
+
+public class RunObject
+{
+    public void Run()
+    {
+        Debug.Log("Run");
+    }
+}
+
+public interface RunInterface
+{
+    void Run();
+}
+
 public class Adapter_Example : MonoBehaviour
 {
+    private RunObject _runObject = new();
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +39,20 @@ public class Adapter_Example : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void Run()
+    {
+        _runObject.Run();
+    }
+}
+
+public class Adapter_Example2 : MonoBehaviour
+{
+    void Start()
+    {
+        // RunObject가 private이므로 adapter로 한번 거쳐서 접속
+        var adapter = new Adapter_Example();
+        adapter.Run();
     }
 }
